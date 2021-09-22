c41de5870f03991335033e799ab7a77ba40a9b5e
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-09-22 14:27:36 -0400

    Created an intro welcome message for RPSLS game

---
+++ Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Foundations
+{
+	class Program
+	{
+		static void Main(string[] args)
+			Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizards, Spock");
+			Console.WriteLine("Select a number between 1 and 5");
+			Console.WriteLine("1.Rock, 2.Paper, 3.Scissors, 4.Lizards, 5.Spock");
+
+		
+			puplic int playerChoice
+			int computerChoice= RandomNumber		
+	}
+}
